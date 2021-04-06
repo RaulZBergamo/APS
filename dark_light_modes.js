07579e3e0d56fde35f43b5ef8084f87a2d463b63
Shaunks <raulbergamo416@gmail.com>
2021-04-05 22:08:32 -0300

    Light dark button, mais perfeito impossivel, só vem de zap

--- dark_light_modes.js
+++ dark_light_modes.js
@@ -1,30 +1,18 @@
-document.getElementById("button").classList.add("button");
-
-document.getElementById("button").classList.remove("button");
-
 function myFunction(){
     if ( document.getElementById("sec-2").classList.contains('dark-theme')){
 
-        document.getElementById("sec-2").classList.remove('dark-theme')
+        document.getElementById("sec-2").classList.remove('dark-theme');
 
-        document.getElementById("sec-2").classList.add('light-theme')
+        document.getElementById("sec-2").classList.add('light-theme');
 
-        if(document.getElementById("div-id").classList.contains('div-p-dark'))
-        {
-            document.getElementById("div-id").classList.remove('div-p-dark')
-            document.getElementById("div-id").classList.add('div-p-light')
-        }
+        document.getElementById("btn_light_dark").innerText = "Claro";
 
     } else {
 
-        document.getElementById("sec-2").classList.remove('light-theme')
+        document.getElementById("sec-2").classList.remove('light-theme');
 
-        document.getElementById("sec-2").classList.add('dark-theme')
+        document.getElementById("sec-2").classList.add('dark-theme');
 
-        if(document.getElementById("div-id").classList.contains('div-p-light')){
-            document.getElementById("div-id").classList.remove('div-p-light')
-            document.getElementById("div-id").classList.add('div-p-dark')
-        }
+        document.getElementById("btn_light_dark").innerText = "Escuro";
     } 
-    
 }
