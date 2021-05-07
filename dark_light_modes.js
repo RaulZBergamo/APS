dfd032785f1dbb7d858f5920a4b82adba6fc8276
otinti <vithor_tinti009@hotmail.com>
2021-05-07 13:31:49 -0300

    Mudança na animação

--- dark_light_modes.js
+++ dark_light_modes.js
@@ -15,4 +15,10 @@ function myFunction(){
 
         document.getElementById("btn_light_dark").innerText = "Claro";
     } 
+
+    if(document.getElementsByClassName("fon").style.color=("blue")){
+
+        document.getElementsByClassName("fon").style.color=("#fcba03");
+
+    }
 }
