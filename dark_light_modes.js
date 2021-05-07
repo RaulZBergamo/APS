84a934e5064357391a8b7386bc31678ee43de0f4
Shaunks <raulbergamo416@gmail.com>
2021-05-06 21:22:39 -0300

    Footer agora troca de cor 'tema'tbm

--- dark_light_modes.js
+++ dark_light_modes.js
@@ -2,8 +2,10 @@ function myFunction(){
     if ( document.getElementById("sec-2").classList.contains('dark-theme')){
 
         document.getElementById("sec-2").classList.remove('dark-theme');
-
         document.getElementById("sec-2").classList.add('light-theme');
+
+        document.getElementById("footer").classList.remove("footer-light");
+        document.getElementById("footer").classList.add('footer-dark')
 
         document.getElementById("btn_light_dark").innerText = "Escuro";
 
@@ -12,6 +14,9 @@ function myFunction(){
         document.getElementById("sec-2").classList.remove('light-theme');
 
         document.getElementById("sec-2").classList.add('dark-theme');
+
+        document.getElementById("footer").classList.remove("footer-dark");
+        document.getElementById("footer").classList.add('footer-light')
 
         document.getElementById("btn_light_dark").innerText = "Claro";
     } 
